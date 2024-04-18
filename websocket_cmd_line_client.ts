952b6e67787a91efd791b32612e605d362256777
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2024-04-18 22:39:17 +0200

    Update websocket_cmd_line_client.ts

--- websocket_cmd_line_client.ts
+++ websocket_cmd_line_client.ts
@@ -1,27 +1,35 @@
-import WebSocket from 'ws';
+// Starts a WebSocket client and connects to a server
 
-const ws = new WebSocket('ws://localhost:8080');
+import WebSocket from "ws";
+
+const port = 3000;
+const ws = new WebSocket(`ws://localhost:${port}`);
 
 ws.on('open', () => {
-console.log('Connected to WebSocket server');
+    console.log('[Client] Connected.');
+    ws.send('Connected to WebSocket server');
 
-// Set up readline interface for command input
-const readline = require('readline').createInterface({
-input: process.stdin,
-output: process.stdout
-});
-
-readline.question('Enter a message: ', (message: string) => {
-ws.send(message);
-console.log(`Sent message: ${message}`);
-readline.close();
 });
 
 ws.on('message', (data: WebSocket.Data) => {
-console.log(`Received message: ${data.toString()}`);
+    if (data.toString() === "input") {
+
+        // Set up a readline interface for command input
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+        
+        readline.question('Enter a message: ', (message: string) => {
+            ws.send(message);
+            readline.close();
+        })
+        
+    } else { 
+        console.log(`Received a message from the server:\n ${data}`);
+    }
 });
 
 ws.on('close', () => {
-console.log('Disconnected from WebSocket server');
-});
+    console.log('Disconnected from WebSocket server');
 });
