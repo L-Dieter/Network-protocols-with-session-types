c67d07615acc8362bef47305f9f8b41a07dad16d
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2024-04-20 01:49:37 +0200

    Update websocket_cmd_line_client.ts
    Add the option to read from the command line multiple times and to close the connection by typing "end"

--- websocket_cmd_line_client.ts
+++ websocket_cmd_line_client.ts
@@ -1,35 +1,51 @@
 // Starts a WebSocket client and connects to a server
 
 import WebSocket from "ws";
+import { setTimeout } from "timers/promises";
+import { randomBytes } from "crypto";
 
 const port = 3000;
 const ws = new WebSocket(`ws://localhost:${port}`);
 
+// Set up a readline interface for command input
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 ws.on('open', () => {
     console.log('[Client] Connected.');
     ws.send('Connected to WebSocket server');
-
+    
 });
 
 ws.on('message', (data: WebSocket.Data) => {
     if (data.toString() === "input") {
-
-        // Set up a readline interface for command input
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
         
         readline.question('Enter a message: ', (message: string) => {
             ws.send(message);
-            readline.close();
+            // readline.close();
         })
         
-    } else { 
+    } 
+    else if (data.toString() === "anotherInput") {
+
+        readline.question('Enter a message: ', (message: string) => {
+            ws.send(message);
+            // readline.close();
+        })
+        
+    }
+    else if (data.toString() === "closeConnection") {
+        readline.close();
+        ws.close();
+    }
+    else { 
         console.log(`Received a message from the server:\n ${data}`);
     }
 });
 
 ws.on('close', () => {
+    readline.close();
     console.log('Disconnected from WebSocket server');
 });
