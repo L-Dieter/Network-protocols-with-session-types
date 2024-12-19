b8589f0c986599221c9f821e10c9b9170740ab88
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2024-12-19 14:58:27 +0100

    Create protocol.ts
    - a separate file to declare the protocol types

---
+++ WebSocketServer/protocol.ts
@@ -0,0 +1,35 @@
+export type Type = 
+    | { type: "number" } 
+    | { type: "string" }
+    | { type: "null" }
+    | { type: "tuple"; payload: Array<Type> }
+    | { type: "array", payload: Type }
+
+export type Dir = "send" | "recv"
+
+export type Label = string
+
+export type Session = 
+        // single operation 
+      { kind: "single"; dir: Dir; payload: Type; program: Program; cont: SessionP}
+        // choice operator with a single program as param
+    // | { kind: "choiceAlt"; dir: Dir; program: Program; alternatives: Record<Label, SessionP>}
+        // choice operaton
+    | { kind: "choice"; dir: Dir; alt_program: Record<Label, Program>; alternatives: Record<Label, SessionP>}
+        // terminate protocol
+    | { kind: "end" }
+
+export type Program = 
+      { command: "send",
+        get_value: () => any,
+        cont: Program }
+    | { command: "recv",
+        put_value: (v: any) => void,
+        cont: Program }
+    | { command: "select",
+        get_value: () => string,
+        cont: Program }
+    | { command: "choose",
+        do_match: (label: Label) => void,
+        alt_cont: Record<Label, Program> }
+    | { command: "end" }
